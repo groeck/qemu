715665d5d93d5495f13dd68c127198b82365e39c
Guenter Roeck <linux@roeck-us.net>
2015-08-10 12:20:44 -0700

    hw/misc/zynq_slcr: Change CPU clock rate for Linux boots
    The Linux kernel only accepts 333334 Khz and 666667 Khz clock rates, and
    may crash if the actual clock rate is too low. The clock rate used to be
    (ps-clk-frequency * 26 / 4), which resulted in a CPU frequency of
    216666 Khz if ps-clk-frequency was set to 33333333 Hz. Change it to
    (ps-clk-frequency * 20 / 2) = 333333 Khz for to make Linux happy.
    Limit the change to Linux boots only.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    
    ---
    v2: Limit scope of change to Linux boots.

--- hw/misc/zynq_slcr.c
+++ hw/misc/zynq_slcr.c
@@ -15,6 +15,7 @@
  */
 
 #include "qemu/osdep.h"
+#include "hw/arm/linux-boot-if.h"
 #include "qemu/timer.h"
 #include "sysemu/runstate.h"
 #include "hw/sysbus.h"
@@ -190,6 +191,8 @@ struct ZynqSLCRState {
 
     MemoryRegion iomem;
 
+    bool is_linux;
+
     uint32_t regs[ZYNQ_SLCR_NUM_REGS];
 
     Clock *ps_clk;
@@ -318,7 +321,10 @@ static void zynq_slcr_reset_init(Object *obj, ResetType type)
 
     s->regs[R_LOCKSTA] = 1;
     /* 0x100 - 0x11C */
-    s->regs[R_ARM_PLL_CTRL]   = 0x0001A008;
+    if (!s->is_linux)
+        s->regs[R_ARM_PLL_CTRL]   = 0x0001A008;
+    else
+        s->regs[R_ARM_PLL_CTRL]   = 0x00014008;
     s->regs[R_DDR_PLL_CTRL]   = 0x0001A008;
     s->regs[R_IO_PLL_CTRL]    = 0x0001A008;
     s->regs[R_PLL_STATUS]     = 0x0000003F;
@@ -327,7 +333,10 @@ static void zynq_slcr_reset_init(Object *obj, ResetType type)
     s->regs[R_IO_PLL_CFG]     = 0x00014000;
 
     /* 0x120 - 0x16C */
-    s->regs[R_ARM_CLK_CTRL]   = 0x1F000400;
+    if (!s->is_linux)
+        s->regs[R_ARM_CLK_CTRL]   = 0x1F000400;
+    else
+        s->regs[R_ARM_CLK_CTRL]   = 0x1F000200;
     s->regs[R_DDR_CLK_CTRL]   = 0x18400003;
     s->regs[R_DCI_CLK_CTRL]   = 0x01E03201;
     s->regs[R_APER_CLK_CTRL]  = 0x01FFCCCD;
@@ -593,21 +602,32 @@ static const VMStateDescription vmstate_zynq_slcr = {
     .version_id = 3,
     .minimum_version_id = 2,
     .fields = (VMStateField[]) {
+        VMSTATE_BOOL(is_linux, ZynqSLCRState),
         VMSTATE_UINT32_ARRAY(regs, ZynqSLCRState, ZYNQ_SLCR_NUM_REGS),
         VMSTATE_CLOCK_V(ps_clk, ZynqSLCRState, 3),
         VMSTATE_END_OF_LIST()
     }
 };
 
+static void zynq_sclr_linux_init(ARMLinuxBootIf *obj,
+                                 bool secure_boot)
+{
+    ZynqSLCRState *s = ZYNQ_SLCR(obj);
+
+    s->is_linux = true;
+}
+
 static void zynq_slcr_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
     ResettableClass *rc = RESETTABLE_CLASS(klass);
+    ARMLinuxBootIfClass *albifc = ARM_LINUX_BOOT_IF_CLASS(klass);
 
     dc->vmsd = &vmstate_zynq_slcr;
     rc->phases.enter = zynq_slcr_reset_init;
     rc->phases.hold  = zynq_slcr_reset_hold;
     rc->phases.exit  = zynq_slcr_reset_exit;
+    albifc->arm_linux_init = zynq_sclr_linux_init;
 }
 
 static const TypeInfo zynq_slcr_info = {
@@ -616,6 +636,10 @@ static const TypeInfo zynq_slcr_info = {
     .parent = TYPE_SYS_BUS_DEVICE,
     .instance_size  = sizeof(ZynqSLCRState),
     .instance_init = zynq_slcr_init,
+    .interfaces = (InterfaceInfo []) {
+        { TYPE_ARM_LINUX_BOOT_IF },
+        { },
+    },
 };
 
 static void zynq_slcr_register_types(void)
