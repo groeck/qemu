fff8b23053ed48dd6fb3c53b0fd397bfa52977ba
Guenter Roeck <linux@roeck-us.net>
2015-09-07 08:38:16 -0700

    PPC: mpc8554ds: Add support for MPC8548CDS
    The configuration for MPC8548CDS is quite similar to MPC8544DS.
    The only real difference is the compatibility statement.
    
    Enables booting 85xx/mpc85xx_cds_defconfig with "-M mpc8548cds".
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/ppc/mpc8544ds.c
+++ hw/ppc/mpc8544ds.c
@@ -49,17 +49,56 @@ static void mpc8544ds_init(MachineState *machine)
     ppce500_init(machine, &params);
 }
 
-
-static QEMUMachine ppce500_machine = {
+static QEMUMachine mpc8544ds_machine = {
     .name = "mpc8544ds",
     .desc = "mpc8544ds",
     .init = mpc8544ds_init,
     .max_cpus = 15,
 };
 
+static void mpc8548cds_fixup_devtree(PPCE500Params *params, void *fdt)
+{
+    const char model[] = "MPC8548CDS";
+    const char compatible[] = "MPC8548CDS\0MPC85xxCDS";
+
+    qemu_fdt_setprop(fdt, "/", "model", model, sizeof(model));
+    qemu_fdt_setprop(fdt, "/", "compatible", compatible,
+                     sizeof(compatible));
+}
+
+static void mpc8548cds_init(MachineState *machine)
+{
+    PPCE500Params params = {
+        .pci_first_slot = 0x1,
+        .pci_nr_slots = 2,
+        .fixup_devtree = mpc8548cds_fixup_devtree,
+        .mpic_version = OPENPIC_MODEL_FSL_MPIC_42,
+        .ccsrbar_base = 0xE0000000ULL,
+        .pci_mmio_base = 0xC0000000ULL,
+        .pci_mmio_bus_base = 0xC0000000ULL,
+        .pci_pio_base = 0xE1000000ULL,
+        .spin_base = 0xEF000000ULL,
+    };
+
+    if (machine->ram_size > 0x80000000) {
+        error_report("The MPC8548CDS board only supports up to 2GB of RAM");
+        exit(1);
+    }
+
+    ppce500_init(machine, &params);
+}
+
+static QEMUMachine mpc8548cds_machine = {
+    .name = "mpc8548cds",
+    .desc = "mpc8548cds",
+    .init = mpc8548cds_init,
+    .max_cpus = 15,
+};
+
 static void ppce500_machine_init(void)
 {
-    qemu_register_machine(&ppce500_machine);
+    qemu_register_machine(&mpc8544ds_machine);
+    qemu_register_machine(&mpc8548cds_machine);
 }
 
 machine_init(ppce500_machine_init);
