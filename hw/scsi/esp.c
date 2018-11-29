c0efc2118a245758609a890a958797405f7b7742
Guenter Roeck <linux@roeck-us.net>
2018-11-29 15:16:36 -0800

    scsi: esp: Don't clear STAT_TC when reading interrupt status
    The status of STAT_TC (transfer count zero) does not change when
    the interrupt status is read, so don't clear it.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -951,7 +951,7 @@ uint64_t esp_reg_read(ESPState *s, uint32_t saddr)
          */
         val = s->rregs[ESP_RINTR];
         s->rregs[ESP_RINTR] = 0;
-        s->rregs[ESP_RSTAT] &= ~STAT_TC;
+        // s->rregs[ESP_RSTAT] &= ~STAT_TC;
         /*
          * According to the datasheet ESP_RSEQ should be cleared, but as the
          * emulation currently defers information transfers to the next TI
