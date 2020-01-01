bbc24eeae32f7d74803c413b1007fa84acad42d4
Guenter Roeck <linux@roeck-us.net>
2020-01-01 13:56:07 -0800

    hcd-ehci: Add optional support for power control
    All we do is to set the "power control" capability bit if
    requested by the implemmentation. Enable it for Chipidea
    hardware.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/usb/hcd-ehci.h
+++ hw/usb/hcd-ehci.h
@@ -261,6 +261,7 @@ struct EHCIState {
     MemoryRegion mem_ports;
     int companion_count;
     bool companion_enable;
+    bool power_control;
     uint16_t capsbase;
     uint16_t opregbase;
     uint16_t portscbase;
@@ -371,6 +372,7 @@ typedef struct SysBusEHCIClass {
     uint16_t opregbase;
     uint16_t portscbase;
     uint16_t portnr;
+    bool power_control;
 } SysBusEHCIClass;
 
 #define FUSBH200_EHCI(obj) \
