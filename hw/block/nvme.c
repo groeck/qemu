9caac0783308e3dd2214e8cf1e18c10a15dbde18
Guenter Roeck <linux@roeck-us.net>
2020-12-20 15:47:51 -0800

    Revert "hw/block/nvme: Verify msix_vector_use() returned value"
    This reverts commit fbf2e5375e33d43c9e1386eed448e1a3c0996e88.
    
    NVME works just fine without MSI-X support.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/block/nvme.c
+++ hw/block/nvme.c
@@ -1368,10 +1368,6 @@ static void nvme_init_cq(NvmeCQueue *cq, NvmeCtrl *n, uint64_t dma_addr,
                          uint16_t cqid, uint16_t vector, uint16_t size,
                          uint16_t irq_enabled)
 {
-    int ret;
-
-    ret = msix_vector_use(&n->parent_obj, vector);
-    assert(ret == 0);
     cq->ctrl = n;
     cq->cqid = cqid;
     cq->size = size;
@@ -1382,6 +1378,7 @@ static void nvme_init_cq(NvmeCQueue *cq, NvmeCtrl *n, uint64_t dma_addr,
     cq->head = cq->tail = 0;
     QTAILQ_INIT(&cq->req_list);
     QTAILQ_INIT(&cq->sq_list);
+    msix_vector_use(&n->parent_obj, cq->vector);
     n->cq[cqid] = cq;
     cq->timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, nvme_post_cqes, cq);
 }
