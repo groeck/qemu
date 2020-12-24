965bf870626b4b63b7c8ff0a2758af6b8a7672d3
BALATON Zoltan <balaton@eik.bme.hu>
2020-12-25 00:23:37 +0100

    ide: Make room for flags in PCIIDEState and add one for legacy mode
    We'll need a flag for implementing some device specific behaviour in
    via-ide but we already have a currently CMD646 specific field that can
    be repurposed for this and leave room for furhter flags if needed in
    the future. This patch changes the "secondary" field to "flags" and
    change CMD646 and its users accordingly and define a new flag for
    forcing legacy mode that will be used by via-ide for now.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- include/hw/ide/pci.h
+++ include/hw/ide/pci.h
@@ -42,6 +42,11 @@ typedef struct BMDMAState {
 #define TYPE_PCI_IDE "pci-ide"
 OBJECT_DECLARE_SIMPLE_TYPE(PCIIDEState, PCI_IDE)
 
+enum {
+    PCI_IDE_SECONDARY, /* used only for cmd646 */
+    PCI_IDE_LEGACY_MODE
+};
+
 struct PCIIDEState {
     /*< private >*/
     PCIDevice parent_obj;
@@ -50,7 +55,7 @@ struct PCIIDEState {
     IDEBus bus[2];
     BMDMAState bmdma[2];
     qemu_irq isa_irq[2];
-    uint32_t secondary; /* used only for cmd646 */
+    uint32_t flags;
     MemoryRegion bmdma_bar;
     MemoryRegion cmd_bar[2];
     MemoryRegion data_bar[2];
