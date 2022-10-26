b98d46aa7e68ced7b85c5d314c25fd31c2c1cea3
Guenter Roeck <linux@roeck-us.net>
2022-10-25 21:41:39 -0700

    hw: misc: pfsoc: Let clock configuration register return something useful
    Return default values provided by hardware, and support
    dummy writes into the various registers.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/misc/mchp_pfsoc_sysreg.c
+++ hw/misc/mchp_pfsoc_sysreg.c
@@ -28,26 +28,32 @@
 #include "hw/sysbus.h"
 #include "hw/misc/mchp_pfsoc_sysreg.h"
 
+#define CLOCK_CONFIG_CR 0x08
+#define RTC_CLOCK_CR    0x0c
+#define SUBBLK_CLOCK_CR 0x84
 #define ENVM_CR         0xb8
 #define MESSAGE_INT     0x118c
 
 static uint64_t mchp_pfsoc_sysreg_read(void *opaque, hwaddr offset,
                                        unsigned size)
 {
-    uint32_t val = 0;
+    MchpPfSoCSysregState *s = opaque;
+    uint32_t val;
 
     switch (offset) {
+    case CLOCK_CONFIG_CR:
+    case RTC_CLOCK_CR:
+    case SUBBLK_CLOCK_CR:
     case ENVM_CR:
-        /* Indicate the eNVM is running at the configured divider rate */
-        val = BIT(6);
+        val = s->reg[offset];
         break;
     default:
+        val = s->reg[offset];
         qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read "
                       "(size %d, offset 0x%" HWADDR_PRIx ")\n",
                       __func__, size, offset);
         break;
     }
-
     return val;
 }
 
@@ -55,15 +61,24 @@ static void mchp_pfsoc_sysreg_write(void *opaque, hwaddr offset,
                                     uint64_t value, unsigned size)
 {
     MchpPfSoCSysregState *s = opaque;
+
     switch (offset) {
     case MESSAGE_INT:
         qemu_irq_lower(s->irq);
         break;
+    case CLOCK_CONFIG_CR:
+    case RTC_CLOCK_CR:
+    case SUBBLK_CLOCK_CR:
+    case ENVM_CR:
+        s->reg[offset] = value;
+        break;
     default:
+        s->reg[offset] = value;
         qemu_log_mask(LOG_UNIMP, "%s: unimplemented device write "
                       "(size %d, value 0x%" PRIx64
                       ", offset 0x%" HWADDR_PRIx ")\n",
                       __func__, size, value, offset);
+        break;
     }
 }
 
@@ -85,12 +100,25 @@ static void mchp_pfsoc_sysreg_realize(DeviceState *dev, Error **errp)
     sysbus_init_irq(SYS_BUS_DEVICE(dev), &s->irq);
 }
 
+static void mchp_pfsoc_sysreg_reset(DeviceState *dev)
+{
+    MchpPfSoCSysregState *s = MCHP_PFSOC_SYSREG(dev);
+
+    memset(s->reg, 0, sizeof(uint32_t) * MCHP_PFSOC_SYSREG_REG_SIZE);
+
+    s->reg[CLOCK_CONFIG_CR] = BIT(4);
+    s->reg[RTC_CLOCK_CR] = 0x00010064;
+    s->reg[SUBBLK_CLOCK_CR] = 0x00000001; /* ENVM enabled */
+    s->reg[ENVM_CR] = 0x40000000 | BIT(6) | 0x0f;
+}
+
 static void mchp_pfsoc_sysreg_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
     dc->desc = "Microchip PolarFire SoC SYSREG module";
     dc->realize = mchp_pfsoc_sysreg_realize;
+    dc->reset = mchp_pfsoc_sysreg_reset;
 }
 
 static const TypeInfo mchp_pfsoc_sysreg_info = {
