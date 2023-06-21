abe974522b894e64924a2ac176023c4ff7961f20
Guenter Roeck <linux@roeck-us.net>
2023-06-20 23:35:14 -0700

    riscv: Generate devicetree only after machine initialization is complete
    If the devicetree is created before machine initialization is complete,
    it misses dynamic devices. Move it into the machine_done function
    to solve the problem.
    
    Fixes: 3029fab643 ("hw/riscv: virt: Add support for generating platform FDT entries")
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -1247,6 +1247,11 @@ static void virt_machine_done(Notifier *notifier, void *data)
     uint32_t fdt_load_addr;
     uint64_t kernel_entry;
 
+    /* create devicetree if not provided */
+    if (!machine->dtb) {
+        create_fdt(s, memmap);
+    }
+
     /*
      * Only direct boot kernel is currently supported for KVM VM,
      * so the "-bios" parameter is not supported when KVM is enabled.
@@ -1519,15 +1524,13 @@ static void virt_machine_init(MachineState *machine)
     }
     virt_flash_map(s, system_memory);
 
-    /* load/create device tree */
+    /* load device tree */
     if (machine->dtb) {
         machine->fdt = load_device_tree(machine->dtb, &s->fdt_size);
         if (!machine->fdt) {
             error_report("load_device_tree() failed");
             exit(1);
         }
-    } else {
-        create_fdt(s, memmap);
     }
 
     s->machine_done.notify = virt_machine_done;
