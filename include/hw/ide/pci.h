646940ac698355d26f267407c871b22516a9ab43
Guenter Roeck <linux@roeck-us.net>
2023-08-05 12:08:35 -0700

    Revert "ide: Make room for flags in PCIIDEState and add one for legacy mode"
    This reverts commit 83c5acda0df0374aee16bbea587f8bc78403ce64.
    
    Does not seem to be needed/used. Drop.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- include/hw/ide/pci.h
+++ include/hw/ide/pci.h
@@ -42,11 +42,6 @@ typedef struct BMDMAState {
 #define TYPE_PCI_IDE "pci-ide"
 OBJECT_DECLARE_SIMPLE_TYPE(PCIIDEState, PCI_IDE)
 
-enum {
-    PCI_IDE_SECONDARY, /* used only for cmd646 */
-    PCI_IDE_LEGACY_MODE
-};
-
 struct PCIIDEState {
     /*< private >*/
     PCIDevice parent_obj;
@@ -55,7 +50,7 @@ struct PCIIDEState {
     IDEBus bus[2];
     BMDMAState bmdma[2];
     qemu_irq isa_irq[2];
-    uint32_t flags;
+    uint32_t secondary; /* used only for cmd646 */
     MemoryRegion bmdma_bar;
     MemoryRegion cmd_bar[2];
     MemoryRegion data_bar[2];
