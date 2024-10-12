de186eb46789909bcdb48d64897c92ac1f2ae3fa
Guenter Roeck <linux@roeck-us.net>
2024-10-11 18:02:01 -0700

    Revert "omap: gpmc: Report WAIT0STATUS=1"
    This reverts commit 4c0febc78a3892525bebde9b4c0b1387a488ea28.
    
    Code removed upstream. Should no longer be needed.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- hw/misc/omap_gpmc.c
+++ hw/misc/omap_gpmc.c
@@ -544,7 +544,7 @@ static uint64_t omap_gpmc_read(void *opaque, hwaddr addr,
         return s->config;
 
     case 0x054:	/* GPMC_STATUS */
-        return 0x101;		/* set WAIT0STATUS, EMPTYWRITEBUFFERSTATUS */
+        return 0x001;
 
     case 0x060 ... 0x1d4:
         cs = (addr - 0x060) / 0x30;
